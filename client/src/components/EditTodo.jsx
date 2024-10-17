2832a7c516d17a514d5359a016bc2ae385a52875
Vito <vitohartanto@mail.ugm.ac.id>
2024-10-17 12:08:28 +0700

    feature: complete Todo done

--- client/src/components/EditTodo.jsx
+++ client/src/components/EditTodo.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { CiEdit } from 'react-icons/ci';
 
 const EditTodo = ({ todo, setDescription, setEditMode }) => {
-  const onEditHandler = (id) => {
+  const onEditHandler = (event, id) => {
+    event.stopPropagation();
     setDescription(todo.description); // Isi input dengan nilai deskripsi dari todo yang ingin diedit.
     setEditMode(id); // Aktifkan mode edit untuk todo dengan ID ini.
   };
 
   return (
-    <button className="mr-6" onClick={() => onEditHandler(todo.todo_id)}>
+    <button
+      className="mr-6"
+      onClick={(event) => onEditHandler(event, todo.todo_id)}
+    >
       <CiEdit />
     </button>
   );
--- client/src/components/ListTodos.jsx
+++ client/src/components/ListTodos.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 import React from 'react';
 import IconCross from '../images/icon-cross.svg';
-
+import IconCheck from '../images/icon-check.svg';
 import EditTodo from './EditTodo';
 
 const ListTodos = ({ description, setDescription, setEditMode }) => {
   const [todos, setTodos] = useState([]);
 
-  const onDeleteHandler = (id) => {
+  const onDeleteHandler = (event, id) => {
+    event.stopPropagation();
     deleteTodo(id);
   };
 
@@ -39,18 +40,70 @@ const ListTodos = ({ description, setDescription, setEditMode }) => {
     getTodos();
   }, []);
 
+  const onCompletedHandler = (id, currentStatus) => {
+    completeTodo(id, currentStatus);
+  };
+
+  const completeTodo = async (id, currentStatus) => {
+    try {
+      const body = { completed: !currentStatus }; // Mengirim status completed sebagai true
+      const completeTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (completeTodo.ok) {
+        // Perbarui state todos setelah berhasil mengubah status di server
+        setTodos((prevTodos) =>
+          prevTodos.map((todo) =>
+            todo.todo_id === id ? { ...todo, completed: !currentStatus } : todo
+          )
+        );
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center ">
-      <div className="rounded-lg z-[2] overflow-hidden">
+      <div className="rounded-lg z-[2] overflow-hidden ">
         {todos.map((todo) => {
           return (
             <div
               key={todo.todo_id}
-              className="relative border-b border-[#777a92] items-center  p-3 w-[327px] bg-[#25273c] text-[#cacde8] flex justify-between"
+              onClick={() => onCompletedHandler(todo.todo_id, todo.completed)}
+              className="hover:cursor-pointer relative border-b border-[#777a92] items-center  p-3 w-[327px] bg-[#25273c] text-[#cacde8] flex justify-between"
             >
-              <p className="absolute border-[#777a92] bg-transparent border-2 w-5 h-5 rounded-full"></p>
+              {todo.completed === false ? (
+                <div
+                  className="absolute border-[#777a92] bg-transparent border-2 w-5 h-5 rounded-full"
+                  onClick={() =>
+                    onCompletedHandler(todo.todo_id, todo.completed)
+                  }
+                />
+              ) : (
+                <div className="absolute bg-gradient-custom border-2 w-5 h-5 rounded-full">
+                  <img
+                    src={IconCheck}
+                    alt="checklist"
+                    className="absolute top-1 left-[3px]"
+                  />
+                </div>
+              )}
+
               <div className="flex w-full justify-between">
-                <p className="pl-8">{todo.description}</p>
+                {todo.completed === false ? (
+                  <p className="pl-8">{todo.description}</p>
+                ) : (
+                  <p className="pl-8">
+                    <s>{todo.description}</s>
+                  </p>
+                )}
+
                 <EditTodo
                   todo={todo}
                   setDescription={setDescription}
@@ -59,7 +112,7 @@ const ListTodos = ({ description, setDescription, setEditMode }) => {
               </div>
               <button
                 className=""
-                onClick={() => onDeleteHandler(todo.todo_id)}
+                onClick={(event) => onDeleteHandler(event, todo.todo_id)}
               >
                 <img src={IconCross} alt="X" />
               </button>
