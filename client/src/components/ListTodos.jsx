d0419fabddd251b29e1dce0b06a9845c34543623
Vito <vitohartanto@mail.ugm.ac.id>
2024-10-17 08:55:26 +0700

    list todos done

--- client/src/components/ListTodos.jsx
+++ client/src/components/ListTodos.jsx
@@ -8,7 +8,9 @@ const ListTodos = () => {
 
   const getTodos = async () => {
     const response = await fetch('http://localhost:5000/todos');
-    const parsedJson = response.json();
+    const parsedJson = await response.json();
+
+    console.log(parsedJson);
 
     setTodos(parsedJson);
   };
@@ -17,17 +19,60 @@ const ListTodos = () => {
     getTodos();
   }, []);
   return (
-    <div className="flex justify-center">
-      <div className="rounded-lg p-3 w-[327px] bg-[#25273c] text-[#cacde8] flex justify-between">
-        <p className="border-[#777a92] bg-transparent border-2 w-5 h-5 rounded-full"></p>
-        <p className="">Jog around the park 3x</p>
-        <button>
-          <CiEdit />
-        </button>
-        <button>
-          <img src={IconCross} alt="X" />
-        </button>
+    <div>
+      {/* yang benar */}
+      <div className="flex flex-col items-center ">
+        <div className="rounded-lg z-[2] overflow-hidden">
+          {todos.map((todo) => {
+            return (
+              <div
+                key={todo.todo_id}
+                className="relative border-b border-[#777a92] items-center  p-3 w-[327px] bg-[#25273c] text-[#cacde8] flex justify-between"
+              >
+                <p className="absolute border-[#777a92] bg-transparent border-2 w-5 h-5 rounded-full"></p>
+                <div className="flex w-full justify-between">
+                  <p className="pl-8">{todo.description}</p>
+                  <button className="mr-6">
+                    <CiEdit />
+                  </button>
+                </div>
+                <button>
+                  <img src={IconCross} alt="X" />
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
+
+      {/* <div className="flex flex-col items-center ">
+        <div className="rounded-lg z-[2] overflow-hidden">
+          <div className="relative border-b border-[#777a92] items-center  p-3 w-[327px] bg-[#25273c] text-[#cacde8] flex justify-between">
+            <p className="absolute border-[#777a92] bg-transparent border-2 w-5 h-5 rounded-full"></p>
+            <div className="flex w-full justify-between">
+              <p className="pl-8">Jog around the park 3x</p>
+              <button className="mr-6">
+                <CiEdit />
+              </button>
+            </div>
+            <button>
+              <img src={IconCross} alt="X" />
+            </button>
+          </div>
+          <div className="relative border-b border-[#777a92] items-center  p-3 w-[327px] bg-[#25273c] text-[#cacde8] flex justify-between">
+            <p className="absolute border-[#777a92] bg-transparent border-2 w-5 h-5 rounded-full"></p>
+            <div className="flex w-full justify-between">
+              <p className="pl-8">Something else</p>
+              <button className="mr-6">
+                <CiEdit />
+              </button>
+            </div>
+            <button>
+              <img src={IconCross} alt="X" />
+            </button>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 };
